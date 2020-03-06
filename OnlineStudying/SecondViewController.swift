0c2dbbf088312ece7184177abe9bbe2ef6707b79
jiehaozhang <309040278@qq.com>
2020-03-06 11:36:15 +0800

    Initial Commit

---
+++ OnlineStudying/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  OnlineStudying
+//
+//  Created by Jiehao Zhang on 2020/3/6.
+//  Copyright © 2020 Jiehao Zhang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
